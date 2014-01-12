2766f84f954a70a6667b9fd1b846c0b818fed4b0
Michal Konturek <michal.konturek@gmail.com>
2014-01-12 00:19:46 +0000

    Moved implementation code.

--- Source/MKKeyboardController.h
+++ Source/MKKeyboardController.h
@@ -8,6 +8,32 @@
 
 #import <Foundation/Foundation.h>
 
-@interface MKKeyboardController : NSObject
+@protocol MKKeyboardControllerDelegate;
+
+@interface MKKeyboardController : NSObject<UITextFieldDelegate>
+
++ (id)controllerWithTextField:(UITextField *)textField;
++ (id)controllerWithTextFields:(NSArray *)textFields;
+
+@property (nonatomic, assign) id<MKKeyboardControllerDelegate> delegate;
+@property (nonatomic, assign) id<UITextFieldDelegate> textFieldDelegate;
+@property (nonatomic, strong) NSArray *textFields;
+
+- (id)initWithTextFields:(NSArray *)textFields;
+
+- (void)moveToNextTextField;
+- (void)moveToPreviousTextField;
+- (void)closeKeyboard;
+
+- (NSInteger)indexForTextField:(UITextField *)textField;
+
+@end
+
+@protocol MKKeyboardControllerDelegate <NSObject>
+
+- (void)controllerDidHideKeyboard:(MKKeyboardController *)controller;
+- (void)controllerDidShowKeyboard:(MKKeyboardController *)controller;
+- (void)controllerWillHideKeyboard:(MKKeyboardController *)controller;
+- (void)controllerWillShowKeyboard:(MKKeyboardController *)controller;
 
 @end
--- Source/MKKeyboardController.m
+++ Source/MKKeyboardController.m
@@ -10,4 +10,138 @@
 
 @implementation MKKeyboardController
 
++ (id)controllerWithTextField:(UITextField *)textField {
+    return [[self alloc] initWithTextFields:@[textField]];
+}
+
++ (id)controllerWithTextFields:(NSArray *)textFields {
+    return [[self alloc] initWithTextFields:textFields];
+}
+
+- (void)dealloc {
+    [self _unsubscribeFromNotifications];
+}
+
+- (id)initWithTextFields:(NSArray *)textFields {
+    self = [super init];
+    if (self) {
+        self.textFields = textFields;
+        [self _setupTextFields];
+        [self _subscribeToNotifications];
+    }
+    return self;
+}
+
+- (id)init {
+    NSString *reason = [NSString stringWithFormat:@"""%@"" Use designated initializer.", NSStringFromSelector(_cmd)];
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:reason userInfo:nil];
+}
+
+- (void)_setupTextFields {
+    for (UITextField *textField in self.textFields) {
+        textField.delegate = self;
+    }
+}
+
+- (void)_subscribeToNotifications {
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self selector:@selector(_onKeyboardDidHide) name:UIKeyboardDidHideNotification object:nil];
+    [center addObserver:self selector:@selector(_onKeyboardDidShow) name:UIKeyboardDidShowNotification object:nil];
+    [center addObserver:self selector:@selector(_onKeyboardWillHide) name:UIKeyboardWillHideNotification object:nil];
+    [center addObserver:self selector:@selector(_onKeyboardWillShow) name:UIKeyboardWillShowNotification object:nil];
+}
+
+- (void)_unsubscribeFromNotifications {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+
+#pragma mark - Notification Handlers
+
+- (void)_onKeyboardDidHide {
+    if (!self.delegate) return;
+    if (![self.delegate conformsToProtocol:@protocol(MKKeyboardControllerDelegate)]) return;
+    [self.delegate controllerDidHideKeyboard:self];
+}
+
+- (void)_onKeyboardDidShow {
+    if (!self.delegate) return;
+    if (![self.delegate conformsToProtocol:@protocol(MKKeyboardControllerDelegate)]) return;
+    [self.delegate controllerDidShowKeyboard:self];
+}
+
+- (void)_onKeyboardWillHide {
+    if (!self.delegate) return;
+    if (![self.delegate conformsToProtocol:@protocol(MKKeyboardControllerDelegate)]) return;
+    [self.delegate controllerWillHideKeyboard:self];
+}
+
+- (void)_onKeyboardWillShow {
+    if (!self.delegate) return;
+    if (![self.delegate conformsToProtocol:@protocol(MKKeyboardControllerDelegate)]) return;
+    [self.delegate controllerWillShowKeyboard:self];
+}
+
+
+#pragma mark - UITextFieldDelegate methods
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    if (!self.textFieldDelegate) return;
+    if (![self.textFieldDelegate respondsToSelector:@selector(textFieldDidBeginEditing:)]) return;
+    [self.textFieldDelegate textFieldDidBeginEditing:textField];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    if (!self.textFieldDelegate) return;
+    if (![self.textFieldDelegate respondsToSelector:@selector(textFieldDidEndEditing:)]) return;
+    [self.textFieldDelegate textFieldDidEndEditing:textField];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField.returnKeyType == UIReturnKeyNext) [self moveToNextTextField];
+    if (textField.returnKeyType == UIReturnKeyDone) [self closeKeyboard];
+    return (textField.returnKeyType == UIReturnKeyDone);
+}
+
+
+#pragma mark - Functionality Methods
+
+- (void)moveToPreviousTextField {
+    for (int i = 0; i < [self.textFields count]; i++) {
+        if ([[self.textFields objectAtIndex:i] isEditing] && i != 0) {
+            [[self.textFields objectAtIndex:i - 1] becomeFirstResponder];
+            break;
+        }
+    }
+}
+
+- (void)moveToNextTextField {
+    for (int i = 0; i < [self.textFields count]; i++) {
+        if ([[self.textFields objectAtIndex:i] isEditing] && i != [self.textFields count] - 1) {
+            [[self.textFields objectAtIndex:i + 1] becomeFirstResponder];
+            break;
+        }
+    }
+}
+
+- (void)closeKeyboard {
+    for (UITextField *textField in self.textFields) {
+        if ([textField isEditing]) {
+            [textField resignFirstResponder];
+            break;
+        }
+    }
+}
+
+- (NSInteger)indexForTextField:(UITextField *)textField {
+    NSInteger index = 0;
+    
+    for (UITextField *t in self.textFields) {
+        if (textField == t) break;
+        index++;
+    }
+    
+    return index;
+}
+
 @end
