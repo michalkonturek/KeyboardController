d3febc9fd5deab1b4daf5958ca4a7e3e90e51a43
Michal Konturek <michal.konturek@gmail.com>
2016-08-04 23:33:54 +0100

    Removes pod folders.

--- Example/Pods/Target Support Files/KeyboardController/KeyboardController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KeyboardController : NSObject
-@end
-@implementation PodsDummy_KeyboardController
-@end
--- Example/Pods/Target Support Files/Pods-KeyboardController_Example/Pods-KeyboardController_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_KeyboardController_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_KeyboardController_ExampleVersionString[];
-
