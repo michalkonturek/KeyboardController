efc1b606e487a24b84524b4ef3f68bb0e6dd8439
Michal Konturek <michal.konturek@gmail.com>
2016-08-04 22:43:00 +0100

    Adds implementation for Example.

--- Example/KeyboardController/ViewController.swift
+++ Example/KeyboardController/ViewController.swift
@@ -8,17 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import KeyboardController
 
+class ViewController: UIViewController {
+    @IBOutlet weak var field1: UITextField?
+    @IBOutlet weak var field2: UITextField?
+    @IBOutlet weak var field3: UITextField?
+    @IBOutlet weak var field4: UITextField?
+    @IBOutlet weak var field5: UITextField?
+    
+    var controller: KeyboardController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fields = [field1!, field2!, field3!, field4!, field5!]
+        self.controller = KeyboardController(fields: fields)
+        self.controller?.delegate = self
+        self.controller?.textFieldDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if (textField == self.field4) { self.moveViewBy(-10) }
+        if (textField == self.field5) { self.moveViewBy(-200) }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if (textField == self.field4) { self.moveViewBy(10) }
+        if (textField == self.field5) { self.moveViewBy(200) }
+    }
+    
+    func moveViewBy(dy: CGFloat) {
+        UIView.animateWithDuration(0.2) { 
+            self.view.frame = CGRectOffset(self.view.frame, 0, dy)
+        }
+    }
+}
+
+extension ViewController: KeyboardControllerDelegate {
+    
+    func controllerDidHideKeyboard(controller: KeyboardController) {
+        
+    }
+    
+    func controllerDidShowKeyboard(controller: KeyboardController) {
+        
+    }
+    
+    func controllerWillHideKeyboard(controller: KeyboardController) {
+        
+    }
+    
+    func controllerWillShowKeyboard(controller: KeyboardController) {
+        
+    }
+}
